8900536243c987a8817d6720dc16a21419730707
Alexey Kureev <a.g.kureev@gmail.com>
2020-02-01 12:22:05 +0000

    An empty UIView component has been added to the project as SwiftUIButton.
    This is the initial setup for the article. Make sure you add your
    developer certificate to the ReactNativeWithSwiftUITutorial target in
    the Signing & Capabilities menu before running it on the device.
    
    When the code runs, you should see a 300x300 red square in the middle of
    your screen's safe area.

---
+++ ios/SwiftUIButtonComponent/SwiftUIButtonManager.m
@@ -0,0 +1,30 @@
+//
+//  SwiftUIButtonManager.m
+//  ReactNativeWithSwiftUITutorial
+//
+//  Created by Alexey Kureev on 01/02/2020.
+//
+
+#import <Foundation/Foundation.h>
+#import "React/RCTViewManager.h"
+
+@interface SwiftUIButtonManager : RCTViewManager
+
+@end
+
+@implementation SwiftUIButtonManager
+
+// Expose module to React Native
+RCT_EXPORT_MODULE()
+
+// Expose count and onCountChange props
+RCT_EXPORT_VIEW_PROPERTY(count, NSNumber);
+RCT_EXPORT_VIEW_PROPERTY(onCountChange, RCTBubblingEventBlock);
+
+- (UIView *)view {
+  // For now, we can just export an empty UIView.
+  // In the later chapters of this tutorial this will be changed.
+  return [[UIView alloc] init];
+}
+
+@end
