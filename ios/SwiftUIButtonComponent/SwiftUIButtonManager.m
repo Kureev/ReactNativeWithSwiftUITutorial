c55414d48627b61978e2ecbb8ab8911d235eb51c
Alexey Kureev <a.g.kureev@gmail.com>
2020-02-01 13:06:27 +0000

    Added SwiftUIButtonProxy to the project.
    SwiftUIButtonProxy class will expose our SwiftUI view (that we will
    create in the next step) to React Native. In this commit we only expose
    a Swift class to RN. In fact, this is the way how you expose Swift
    components to React Native. Once you create a Swift file in your React
    Native project, XCode will ask you if you'd like to create a bridging
    header. Say "yes" as this header auto-bridges objc and swift realms.
    Without it, we won't be able to return a SwiftUIProxy from the -(UIView
    *)view {...} method.
    
    P.S. Mind the "<ProjectName>-Swift.h" file in the Manager. It brings the
    Swift realm to Objective-C.

--- ios/SwiftUIButtonComponent/SwiftUIButtonManager.m
+++ ios/SwiftUIButtonComponent/SwiftUIButtonManager.m
@@ -7,6 +7,7 @@
 
 #import <Foundation/Foundation.h>
 #import "React/RCTViewManager.h"
+#import "ReactNativeWithSwiftUITutorial-Swift.h"
 
 @interface SwiftUIButtonManager : RCTViewManager
 
@@ -24,7 +25,7 @@ RCT_EXPORT_VIEW_PROPERTY(onCountChange, RCTBubblingEventBlock);
 - (UIView *)view {
   // For now, we can just export an empty UIView.
   // In the later chapters of this tutorial this will be changed.
-  return [[UIView alloc] init];
+  return [[SwiftUIButtonProxy alloc] init];
 }
 
 @end
---
+++ ios/SwiftUIButtonComponent/SwiftUIButtonProxy.swift
@@ -0,0 +1,12 @@
+//
+//  SwiftUIButtonProxy.swift
+//  ReactNativeWithSwiftUITutorial
+//
+//  Created by Alexey Kureev on 01/02/2020.
+//
+
+import UIKit
+
+final class SwiftUIButtonProxy : UIView {
+  
+}
